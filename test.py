afae86876cacc651f26cc3bc9cad5034f6365ce3
Andrew Liu <75716016+andaloo23@users.noreply.github.com>
2025-09-24 18:06:49 -0500

    fix urdf

--- test.py
+++ test.py
@@ -1,23 +1,40 @@
+import os
 from isaacsim import SimulationApp
 
 simulation_app = SimulationApp({"headless": False})
 
 from omni.isaac.core import World
-from omni.isaac.core.utils.nucleus import get_assets_root_path
-from omni.isaac.core.utils.stage import add_reference_to_stage
-from omni.isaac.urdf import _urdf
-import os
+from omni.kit.commands import execute as omni_kit_commands_execute
+from omni.importer.urdf import _urdf
+from omni.isaac.core.utils.extensions import enable_extension
+
+enable_extension("omni.importer.urdf")
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+print(f"Current directory: {current_dir}")
 
 world = World()
 
-urdf_path = os.path.join(os.path.dirname(__file__), "so100.urdf")
+urdf_path = os.path.join(current_dir, "so100.urdf")
 
-result, prim_path = _urdf.acquire_urdf_interface().parse_urdf(
+import_config = _urdf.ImportConfig()
+import_config.import_inertia_tensor = True
+import_config.fix_base = True
+import_config.make_default_prim = True
+import_config.create_physics_scene = True
+import_config.search_path = [os.path.join(current_dir, "assets")]
+import_config.default_drive_type = _urdf.UrdfJointTargetType.JOINT_DRIVE_POSITION
+
+robot_prim_path = "/World/so100_robot"
+
+result, prim_path = omni_kit_commands_execute(
+    "URDFParseAndImportFile",
     urdf_path=urdf_path,
-    import_inertia_tensor=True,
-    fix_base=True,
-    make_default_prim=True,
-    create_physics_scene=True
+    import_config=import_config,
+    dest_path=robot_prim_path,
+    get_articulation_root=True
 )
 
-world.reset()
+world.play()
+simulation_app.run()
+simulation_app.shutdown()
